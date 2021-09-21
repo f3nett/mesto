43149dbe80358d5db7962853c86f99c36caf0140
vkostina <v.kostina@city-mobil.ru>
2021-09-22 00:01:32 +0300

    resolve errors after review

--- scripts/index.js
+++ scripts/index.js
@@ -8,43 +8,27 @@ let inputDescription = popup.querySelector('.popup__description');
 let profileName = document.querySelector('.profile-info__name');
 let profileDescription= document.querySelector('.profile-info__description');
 
-function popupToggle() {
-    popup.classList.toggle('popup_opened');
-    page.classList.toggle('page_no-scroll');
-}
-
 function popupOpen() {
-    if (popup.classList.contains('popup_opened') === true) {
-        return;
-    } else {
-        popupToggle();
+    if (popup.classList.contains('popup_opened') !== true) {
+        popup.classList.add('popup_opened');
+        page.classList.add('page_no-scroll');
+        inputName.value = profileName.textContent;
+        inputDescription.value = profileDescription.textContent;
     }
 }
 
 function popupClose() {
-    if (popup.classList.contains('popup_opened') === true) {
-        inputName.value = profileName.textContent;
-        inputDescription.value = profileDescription.textContent;
-        popupToggle();
-    }
-}
-
-function clickOverlay(event) {
-    if (event.target === event.currentTarget) {
-        popupClose();
-    }
+        popup.classList.remove('popup_opened');
+        page.classList.remove('page_no-scroll');
 }
 
 function submitForm (event) {
     event.preventDefault();
-    let name = inputName.value;
-    let description = inputDescription.value;
-    profileName.textContent = name;
-    profileDescription.textContent = description;
-    popupToggle();
+    profileName.textContent = inputName.value;
+    profileDescription.textContent = inputDescription.value;
+    popupClose();
 }
 
 popupOpenBtn.addEventListener('click', popupOpen);
 popupCloseBtn.addEventListener('click', popupClose);
-popup.addEventListener('click', clickOverlay);
 form.addEventListener('submit', submitForm);
