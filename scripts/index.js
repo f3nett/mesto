8666bf596610ff5fbb9dfda88acaa8b09003292e
vkostina <v.kostina@city-mobil.ru>
2021-09-22 10:11:04 +0300

    resolve errors after review

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,12 @@
 let page = document.querySelector('.page')
 let popup = document.querySelector('.popup');
 let form = document.querySelector('.popup__form');
-let popupOpenBtn = document.querySelector('.profile-info__edit-button');
+let popupOpenBtn = document.querySelector('.profile__edit-button');
 let popupCloseBtn = popup.querySelector('.popup__close-button');
 let inputName = popup.querySelector('.popup__name');
 let inputDescription = popup.querySelector('.popup__description');
-let profileName = document.querySelector('.profile-info__name');
-let profileDescription= document.querySelector('.profile-info__description');
+let profileName = document.querySelector('.profile__name');
+let profileDescription= document.querySelector('.profile__description');
 
 function popupOpen() {
     if (popup.classList.contains('popup_opened') !== true) {
